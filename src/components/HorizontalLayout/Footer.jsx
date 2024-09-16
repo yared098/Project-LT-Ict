ad0e366ee65db44a4e0a156fc7c8ed2ee76a34fd
yared <fdessalew@gmail.com>
2024-09-17 00:28:35 +0300

    remove old footer and head

--- src/components/HorizontalLayout/Footer.jsx
+++ src/components/HorizontalLayout/Footer.jsx
@@ -7,10 +7,10 @@ const Footer = () => {
       <footer className="footer">
         <Container fluid={true}>
           <Row>
-            <Col md={6}>{new Date().getFullYear()} © Skote.</Col>
+            <Col md={6}>{new Date().getFullYear()} © lt ict.</Col>
             <Col md={6}>
               <div className="text-sm-end d-none d-sm-block">
-                Design & Develop by Themesbrand
+                LT ICT Solution
               </div>
             </Col>
           </Row>
--- src/components/VerticalLayout/Footer.jsx
+++ src/components/VerticalLayout/Footer.jsx
@@ -7,10 +7,10 @@ const Footer = () => {
       <footer className="footer">
         <Container fluid={true}>
           <Row>
-            <Col md={6}>{new Date().getFullYear()} © Skote.</Col>
+            <Col md={6}>{new Date().getFullYear()} © LT ICT.</Col>
             <Col md={6}>
               <div className="text-sm-end d-none d-sm-block">
-                Design & Develop by Themesbrand
+                Design & Develop by LT ICT Solution
               </div>
             </Col>  
           </Row>
